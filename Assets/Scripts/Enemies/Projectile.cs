756c8b47a38eeac9398e247714cbdaf10e302a42
Jordan Tobin <jordantobin123@hotmail.com>
2025-11-23 12:04:50 -0800

    Turret and spawn gates functional, almost finished with prototype

--- Assets/Scripts/Enemies/Projectile.cs
+++ Assets/Scripts/Enemies/Projectile.cs
@@ -1,40 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {   
+    // Functionality for the projectiles fired from turret.
     PlayerHealth playerHealth;
     int damage = 2; 
-    
-    [SerializeField] float speed = 5f; 
+    float speed = 30f; 
 
-    [SerializeField] Rigidbody projectile;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] ParticleSystem hitVFX; 
 
-    void Update() 
-    {
-        FireProjectile(); 
-    }
-    void FireProjectile()
+    void Start()
     {   
-        // Use the velocity not other forces, this gives the best physics effect.
-        // note frame rate independance. 
-        projectile.velocity = transform.forward * speed * Time.deltaTime; 
+        // Set movement speed once, through velocity not forces, this gives most realistic effect.
+        rb.velocity = transform.forward * speed;
     }
 
     void OnTriggerEnter(Collider other) 
-    {
+    {   
+        // Projectiles contain a trigger
+        // Damage player if hit
         playerHealth = other.GetComponent<PlayerHealth>();
 
         if (playerHealth)
         {
             playerHealth.TakeDamage(damage); 
         }
+
+        // Then initialize small explosion and destroy self. 
+        Instantiate(hitVFX, transform.position, Quaternion.identity);
+        Destroy(gameObject); 
     }
 
-    public void Init(int damage)
-    {
-        this.damage = damage; 
+    public void Init(int damage, float speed)
+    {   
+        // Initalize serialized values from the turret, keeping things tidy. 
+        this.damage = damage;
+        this.speed = speed;  
     }
 }
--- Assets/Scripts/Enemies/Turret.cs
+++ Assets/Scripts/Enemies/Turret.cs
@@ -9,15 +9,22 @@ public class Turret : MonoBehaviour
     [SerializeField] GameObject projectilePrefab; 
 
     // Initialize this to the barrel of the turret (not the base)
+    // Allows for rotation of head only. 
     [SerializeField] Transform turretHead; 
 
     // Initialize this to the players camera root
     [SerializeField] Transform playerTargetPoint;
+    
+    // Fire point for projectiles
     [SerializeField] Transform projectileSpawnPoint; 
 
     [SerializeField] float fireRate = 2f; 
 
     [SerializeField] int damage = 2; 
+
+    // The speed with which individual projectiles fire
+    // Too high == tunelling, so be careful
+    [SerializeField] float speed = 30f; 
 
     PlayerHealth player; 
 
@@ -40,8 +47,11 @@ public class Turret : MonoBehaviour
         {   
             // Instantiate a projectile at the spawn point. 
             yield return new WaitForSeconds(fireRate);
+            // At the same time, get the script of the projectils to initialize its damage and speed (pass values through Init method)
             Projectile newProjectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, turretHead.rotation).GetComponent<Projectile>();
-            newProjectile.Init(damage);
+            newProjectile.Init(damage, speed);
+            // Change rotation due to parallax error, the projectile will spawn facing from the pivot point, until corrected by LookAt
+            // See parallax error in rifling. 
             newProjectile.transform.LookAt(playerTargetPoint);
         }
     }
