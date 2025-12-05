a016994aa74e9746cf314f3de930e48dfc9fe819
Cameron Lee <7cameronlee@gmail.com>
2025-12-05 01:52:08 -0800

    Basic procedural terrain setup, perlin noise tweaking next

--- Assets/Scripts/Map/MeshGenerator.cs
+++ Assets/Scripts/Map/MeshGenerator.cs
@@ -2,17 +2,115 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter))]
 public class MeshGenerator : MonoBehaviour
 {
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+
+    //vertex count = (xSize + 1) * (zSize + 1)
+    public int xSize = 20;
+    public int zSize = 20;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        StartCoroutine(CreateShape());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        UpdateMesh();
+    }
+
+    IEnumerator CreateShape()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+        float[] vertY = new float[(xSize + 1) * (zSize + 1)];
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertY[i] = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
+                vertices[i] = new Vector3(x, 0, z);
+                i++;
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+
+                yield return new WaitForSeconds(0.01f);
+            }
+            vert++;
+        }
+
+        //demo animation remove vertY and set y directly for instant
+        float duration = 1.5f;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / duration);
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                float y = Mathf.Lerp(0f, vertY[i], t);
+                vertices[i] = new Vector3(vertices[i].x, y, vertices[i].z);
+            }
+
+            mesh.vertices = vertices;
+            mesh.RecalculateNormals();
+            yield return null;
+        }
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = new Vector3(vertices[i].x, vertY[i], vertices[i].z);
+        }
+
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+    private void OnDrawGizmos()
+    {
+
+        if (vertices == null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], .1f);
+        }
     }
 }
+
